388e9e9776c1bc442b44df961704f43b9aaa847c
Machanga <florenceshiru765@gmail.com>
2019-07-19 16:26:33 +0300

    add tests for the models to ensure theyr are working as expected

--- mygallery/models.py
+++ mygallery/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    '''
-    image class for all images added to the application
-    '''
-    image = models.ImageField(upload_to='images/')
-    title = models.CharField(max_length=60)
-    description = models.CharField(max_length=300, blank = True)
-    post_date = models.DateTimeField(auto_now_add=True)
-    category = models.ManyToManyField(category)
-    location = models.ForeignKey(location)
-
-    def __str__(self):
-        return self.title
-
 class location(models.Model):
     '''
     This is a class for the location of where the images were taken
@@ -31,3 +17,17 @@ class category(models.Model):
     name = models.CharField(max_length = 30)
     def __str__(self):
         return self.name
+class Image(models.Model):
+    '''
+    image class for all images added to the application
+    '''
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=300, blank = True)
+    post_date = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(category)
+    location = models.ForeignKey(location)
+
+    def __str__(self):
+        return self.title
+
--- mygallery/tests.py
+++ mygallery/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import location, category, Image
 
 # Create your tests here.
+class locationTestClass(TestCase):
+    def setUp(self):
+        # Creating a new location and saving it
+        self.location = location(name = 'Nairobi')
+        self.location.save()
+    
+class categoryTestClass(TestCase):
+    def setUp(self):
+    # Creating a new category and saving it
+        self.category = category(name = 'Testcategory')
+        self.category.save()
+
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.image= Image(image = "/images", name = "TestImage", description = "A image for testing", location =self.location, category="thrilling" )
+        self.image.save_image()
+        # Testing  instance
+

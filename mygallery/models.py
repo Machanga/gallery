6ef3ab4d514b3c5ff1bbc5bfca61078dc815c0e8
Machanga <florenceshiru765@gmail.com>
2019-07-19 16:06:06 +0300

    create the location model class that will create the location table in the database and store the information on the various locations that the images were taken

--- mygallery/models.py
+++ mygallery/models.py
@@ -3,11 +3,17 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     '''
-    image Class for all images added to the application
+    image class for all images added to the application
     '''
     image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=60)
     description = models.CharField(max_length=300, blank = True)
     post_date = models.DateTimeField(auto_now_add=True)
-    category = models.ManyToManyField(category)
-    location = models.ForeignKey(location)
+    # category = models.ManyToManyField(category)
+    # location = models.ForeignKey(location)
+
+class location(models.Model):
+    '''
+    This is a class for the location of where the images were taken
+    '''
+    name = models.CharField(max_length = 30)

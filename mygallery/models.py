b60bd162c20cac506e6c627666fcbe5783ac1928
Machanga <florenceshiru765@gmail.com>
2019-07-19 15:58:42 +0300

    create the image model class that holds the fields of the image table

--- mygallery/models.py
+++ mygallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    '''
+    image Class for all images added to the application
+    '''
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=300, blank = True)
+    post_date = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(category)
+    location = models.ForeignKey(location)

6ccabefd6a13fdf4d5eb0659af9e958af1aae734
Machanga <florenceshiru765@gmail.com>
2019-07-21 17:12:19 +0300

    add the modal that will display the image details once a user clicks on an image

--- mygallery/admin.py
+++ mygallery/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import location, category, Image
+from .models import Location, Category, Image
 
 # Register your models here.
 admin.site.register(Image)
-admin.site.register(location)
-admin.site.register(category)
+admin.site.register(Location)
+admin.site.register(Category)
 
--- mygallery/urls.py
+++ mygallery/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index, name='index'),
+    url(r'^location/(\d+)',views.display_location,name='display_location'),
     url(r'^search/', views.search_results, name="search_results")
 ]
 
